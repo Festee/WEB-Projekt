32a44306bc124dbdee76f1e2f08da5dae5e57126
Festim Dibrani <fd61273@ubt-uni.net>
2022-12-25 03:39:26 +0100

    JAVASCRIPT Code

--- script.js
+++ script.js
@@ -45,8 +45,87 @@ var swiper = new Swiper(".home-slider", {
   }
   
 
+  function validate(){
+    var emri=document.getElementById("name").value;
+    var regex=/^[a-zA-Z]{3,}$/
 
+    if(emri.trim()==""){
+      alert("Shkruaj emrin: ");
+    }
+    else if((regex.test(emri))){
+      alert("Emri pranohet");
+    }
+    else{
+      alert("Emri nuk pranohet");
+    }
+
+    var emaili=document.getElementById("email").value;
+    var regex=/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/
+
+    if(emaili.trim()==""){
+      alert("Shkruaj nje email: ");
+    }
+    else if((regex.test(emaili))){
+      alert("Email pranohet");
+    }
+    else{
+        alert("Email nuk pranohet");
+    }
+
+    var numri=document.getElementById("nrtel").value;
+    var regex =/^0+4+[3-5 | 8-9]+[0-9]{6}$/
+
+  if(numri.trim() == ""){
+    alert("Shkruaj numrin e telefonit");
+  } 
+   else if((regex.test(numri))){
+       alert("Numri i telefonit pranohet");
+   }
   
+   else{
+      alert("Numri i telefonit nuk pranohet");
+  }
+
+   var adresa=document.getElementById("address").value;
+   var regex=/^[a-zA-Z]+ [a-zA-Z]+,[0-9]{5}$/
+
+   if(adresa.trim()==""){
+    alert("Shkruaj adresen ");
+   }
+   else if((regex.test(adresa))){
+    alert("Adresa pranohet");
+   }
+   else{
+    alert("Adresa nuk pranohet");
+   }
+
+   var lokacioni=document.getElementById("location").value;
+   var regex=/[A-Za-z]/
+
+   if(lokacioni.trim()==""){
+    alert("Shkruaj nje lokacion qe do vizitosh: ");
+   }
+   else if((regex.test(lokacioni))){
+    alert("Lokacioni pranohet");
+   }
+   else{
+    alert("Lokacioni nuk pranohet");
+   }
+
+   var persona=document.getElementById("persona").value;
+   var regex=/[1-9][0-9]/
+
+   if(persona.trim()==""){
+    alert("Shkruaj numrin e personave: ");
+   }
+   else if((regex.test(persona))){
+    alert("Numri i personave pranohet");
+   }
+   else{
+    alert("Nuk pranohet numri personave");
+   }
+  }
+
 
   function validoLogIn(){
     var emaili=document.getElementById("email").value;
@@ -76,3 +155,4 @@ var swiper = new Swiper(".home-slider", {
     }
   
   }
+
