edd50a6d0bd37c679ed7ae3f7bfa2ea45fe35796
AltinMurseli <96544057+AltinMurseli@users.noreply.github.com>
2023-02-26 13:17:52 +0100

    Userat.php
    Php code-Userat

---
+++ Userat.php
@@ -0,0 +1,32 @@
+<?php
+
+    $user1=[
+        "name"=>"Festim",
+        "surname"=>"Dibrani",
+        "email"=>"fd61273@ubt-uni.net",
+        "username"=>"festimdibrani",
+        "password"=>"Dibrani123",
+        "role"=>"admin"
+    ];
+
+    $user2=[
+        "name"=>"Altin",
+        "surname"=>"Murseli",
+        "email"=>"altin.murseli@ubt-uni.net",
+        "username"=>"altinmurseli",
+        "password"=>"Altin123",
+        "role"=>"user"
+    ];
+
+    $user3=[
+        "name"=>"Greta",
+        "surname"=>"Ahma",
+        "email"=>"greta.ahma@ubt-uni.net",
+        "username"=>"gretaahma",
+        "password"=>"Greta123",
+        "role"=>"user"
+    ];
+
+    $users=[$user1,$user2,$user3];
+
+?>
